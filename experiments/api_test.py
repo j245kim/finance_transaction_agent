1e4c1f4f661a6169dc0b30cc2097dff47a5bebe2
Lee Seung Hun <zeromiles1123@gmail.com>
2025-01-23 17:39:38 +0900

    "에라 모르겠다."

--- experiments/api_test.py
+++ experiments/api_test.py
@@ -1,21 +1,32 @@
 import os
 
 from dotenv import load_dotenv
-from langchain_teddynote import logging
 
-# 프로젝트 이름을 입력합니다.
-logging.langsmith("TEST")
+
+load_dotenv()
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+
+
+from langchain_core.prompts import PromptTemplate
+
+# template 정의
+template = "{country}의 수도는 어디인가요?"
+
+# from_template 메소드를 이용하여 PromptTemplate 객체 생성
+prompt_template = PromptTemplate.from_template(template)
+prompt_template
+
+# prompt 생성
+prompt = prompt_template.format(country="대한민국")
+prompt
+
+# prompt 생성
+prompt = prompt_template.format(country="미국")
+prompt
 
 from langchain_openai import ChatOpenAI
 
-# 객체 생성
-llm = ChatOpenAI(
-    temperature=0.1,  # 창의성 (0.0 ~ 2.0)
-    model="gpt-4o-mini",  # 모델명
+model = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0.1,
 )
-
-# 질의내용
-question = "대한민국의 수도는 어디인가요?"
-
-# 질의
-print(f"[답변]: {llm.invoke(question)}")
